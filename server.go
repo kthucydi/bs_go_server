3956c5be902a4d6e40f75e3b7707b1718d06eeff
Anton Bryntsev <anton.bryntsev@outlook.com>
2024-08-07 17:47:20 +0300

    tune prefix

--- server.go
+++ server.go
@@ -100,7 +100,7 @@ func (backServer *BackServerType) SetEndPoint(API APISettings) {
 	}
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	// Print Allowed methods in headers
 	if value, ok := backServer.Cfg["USE_INNER_CORS"]; ok && value == "true" {
